f3ea600f4e0285d90d65fb0758b357d7e693f11f
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-04-09 16:04:52 +0400

    My test commit 3

--- program.c
+++ program.c
@@ -7,3 +7,26 @@ int main()
 	printf("Hello, world\n");
 
 }
+
+
+#include <stdio.h>
+
+/* print Fahrenheit-Celsius table for far = 0, 20, ..., 300 */
+
+int main()
+{
+	int far, celsius;
+	int lower, upper, step;
+
+	lower =0;	/*lower limit of temperature scale */
+	upper = 300;	/* upper limit */
+	step = 20;	/* step size */
+
+	far = lower;
+	while (far <= upper) {
+		celsius = 5 * (fahr-32) / 9;
+		printf("%d\t%d\n", fahr, celsius);
+		fahr = fahr + step;
+	}
+}
+
