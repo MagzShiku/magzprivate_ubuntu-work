2382ee24e5d918a7c2c65b8aa04750125bd04f1d
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-04-10 05:14:20 +0400

    my 2nd C code

--- program.c
+++ program.c
@@ -1,30 +1,19 @@
 #include <stdio.h>
 
-int main()
-
-{
-	
-	printf("Hello, world\n");
-
-}
-
-
-#include <stdio.h>
-
 /* print Fahrenheit-Celsius table for far = 0, 20, ..., 300 */
 
 int main()
 {
-	int far, celsius;
+	int fahr, celsius;
 	int lower, upper, step;
 
 	lower =0;	/*lower limit of temperature scale */
 	upper = 300;	/* upper limit */
 	step = 20;	/* step size */
 
-	far = lower;
-	while (far <= upper) {
-		celsius = 5 * (fahr-32) / 9;
+	fahr = lower;
+	while (fahr <= upper) {
+		celsius = 5 * (fahr - 32) / 9;
 		printf("%d\t%d\n", fahr, celsius);
 		fahr = fahr + step;
 	}
