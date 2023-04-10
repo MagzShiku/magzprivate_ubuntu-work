39707d6c090b6ef86c1955916c32ac02694c59c4
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-04-10 05:32:51 +0400

    3rd excercise in C

--- program.c
+++ program.c
@@ -7,14 +7,14 @@ int main()
 	int fahr, celsius;
 	int lower, upper, step;
 
-	lower =0;	/*lower limit of temperature scale */
+	lower = 0;	/*lower limit of temperature scale */
 	upper = 300;	/* upper limit */
 	step = 20;	/* step size */
 
 	fahr = lower;
 	while (fahr <= upper) {
 		celsius = 5 * (fahr - 32) / 9;
-		printf("%d\t%d\n", fahr, celsius);
+		printf("%10d %15d\n", fahr, celsius);
 		fahr = fahr + step;
 	}
 }
