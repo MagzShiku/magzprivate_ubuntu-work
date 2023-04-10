5595a229ea2173aeef8033c2f7c58e9e34c1e581
Magdaline Njuguna <m2015njuguna@gmail.com>
2023-04-10 08:31:08 +0400

    3 Functions

--- program.c
+++ program.c
@@ -1,12 +1,5 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <libgen.h>
-#include <errno.h>
-#include <string.h>
-#include <getopt.h>
-#include <sys/types.h>
-
 
 /* print Fahrenheit-Celsius table for far = 0, 20, ..., 300 */
 
